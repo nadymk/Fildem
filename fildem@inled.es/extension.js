606dd599fca968b78a3b935e9394fa104783a9ea
Nadeem Khondokar <nadymk@users.noreply.github.com>
2026-07-23 11:23:08 +0100

    restore stable panel extension while native frontend is corrected

--- fildem@inled.es/extension.js
+++ fildem@inled.es/extension.js
@@ -10,7 +10,9 @@ import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
 const BUS = 'es.inled.fildem';
 const PATH = '/es/inled/fildem';
-const IFACE = 'es.inled.fildem';
+// Fildem's historical MyService methods/signals use the registrar interface
+// name even though they live on the es.inled.fildem object.
+const IFACE = 'com.canonical.AppMenu.Registrar';
 
 class NativeMenuManager {
     constructor() {
